067d323f6bffb56e7fd425142eb70e4e38ea6bff
Apollo Chiu <eldorado.tw@gmail.com>
2019-12-08 18:38:06 +0800

    add filter reviews less than 100 words

--- read0.py
+++ read0.py
@@ -11,3 +11,9 @@ length = 0
 for d in data:
 	length += len(d)
 print('平均留言長度為: ', length / len(data), '個字')
+
+filter1 = []
+for d in data:
+	if len(d) < 100:
+		filter1.append(d)
+print('留言少於100個字的共有: ', len(filter1), '筆')
