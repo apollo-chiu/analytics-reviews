8c143f7d98eb45f98e0599c7c924b12d7f88ea0c
Apollo Chiu <eldorado.tw@gmail.com>
2019-12-08 18:23:37 +0800

    calc average length of reviews

--- read0.py
+++ read0.py
@@ -5,7 +5,9 @@ with open('reviews.txt', 'r') as f:
 		data.append(line)
 		if len(data) % 10000 == 0:
 			print(len(data))
-print(len(data))
-print(data[0])
-print('---------------------------')
-print(data[1])
+print('檔案讀取完了, 總共有', len(data), '筆資料')
+
+length = 0
+for d in data:
+	length += len(d)
+print('平均留言長度為: ', length / len(data), '個字')
